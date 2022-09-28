7c46a1c944d11867ac9f488ffae35393413b721b
AungKyawPhyo1142 <aungkyawphyo22233@gmail.com>
2022-09-28 13:03:51 +0630

    Filter Tasks when done

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,16 @@
         <div class="col-2 fs-2">Done</div>
       </div>
     
-      <div class="row fs-4 p-1 mb-1 bg-secondary border rounded text-white" v-for="(task,index) in tasks" v-bind:key="index">
-        <div class="col" :class="task.done ? 'delete':''">{{task.action}}</div>
+      <div class="row fs-4 p-1 mb-1 bg-secondary border rounded text-white" v-for="(task,index) in filterTasks" v-bind:key="index">
+        <div class="col" :class=" task.done ? 'delete':'' ">{{task.action}}</div>
         <div class="col-2"><input type="checkbox" class="" v-model="task.done"></div>
       </div>
-    
+
+      <div class="row mt-4 d-flex justify-content-between">
+        <button class="col-3 btn btn-danger" v-bind="hideCompleted" @click="hideCompletedItems()">Hide Complete Tasks</button>
+        <button class="col-3 btn btn-primary"  v-bind="hideCompleted" @click="showCompletedItems()">Show Complete Tasks</button>
+      </div>
+
     </div>
   </div>
 </template>
@@ -21,17 +26,37 @@
 <script>
 
 export default{
+  
   name: "App", // name of the component
   
   data: ()=> ({
     name:'Sebastian',
+    hideCompleted: false,
     tasks: [
       {'action': 'Buy new phone','done': false},
       {'action': 'Buy new shoe','done': true},
       {'action': 'Wash clothes','done': false},
       {'action': 'Wash Dishes','done': true}
     ]
-  })
+  }),
+
+  computed: {
+  
+    filterTasks(){
+      return this.hideCompleted ? this.tasks.filter(item => !item.done) : this.tasks;      
+    }
+
+  },
+
+  methods: {
+    hideCompletedItems(){
+      this.hideCompleted=true;
+    },
+    showCompletedItems(){
+      this.hideCompleted=false;
+    }
+  }
+
 }
 
 </script>
